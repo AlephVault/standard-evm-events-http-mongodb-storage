f2792d96ec0c779a83929a7df705765bb66555d4
Luis Masuelli <luis@datagran.io>
2022-06-25 17:54:45 -0500

    Adding the event handler class and the validator.

---
+++ alephvault/evm_http_storage/core/event_handler.py
@@ -0,0 +1,27 @@
+from pymongo import MongoClient
+from pymongo.client_session import ClientSession
+from web3.datastructures import AttributeDict
+
+
+class EventHandler:
+    """
+    A handler for a given event being received. It will take
+    into account 3 things when an event is received: a client,
+    a session (within that client), and an event to process.
+    """
+
+    def __call__(self, client: MongoClient, session: ClientSession, event: AttributeDict):
+        """
+        Processes an event inside a transaction. That transaction exists in a session,
+        which is turn belongs to the client, and the given event will be processed by
+        said transaction. This handler must process whatever it needs as given (e.g.
+        for ERC-20 Transfer event, increment and decrement existing quantities, save
+        for the fact of 0x0 addresses), and return whatever makes sense for the game
+        to be notified about the changes.
+        :param client: The MongoDB client to use.
+        :param session: The current MongoDB session.
+        :param event: The event being processed.
+        :return: Whatever makes sense for the game.
+        """
+
+        raise NotImplementedError
