89603fddd7efa2e18abfed7ccd45614514e8bac2
Luis Masuelli <luis@datagran.io>
2022-06-25 09:31:03 -0500

    A fix on the events schema.

--- alephvault/evm_http_storage/schemas/events.py
+++ alephvault/evm_http_storage/schemas/events.py
@@ -1,3 +1,16 @@
+"""
+These schemas define the configuration of event settings.
+Each event contains an ABI (which is converted to a list
+  by using `json.loads(abi)` over the original string
+  value and using that result in the spec itself - it
+  will be converted to string later when needed to be
+  used as argument), a contract address (a valid, but
+  not checksum-verified, hexadecimal address), and the
+  handler that will be used to process each event in the
+  list of events of a given block.
+"""
+
+
 EVENT = {
     "address": {
         "type": "string",
