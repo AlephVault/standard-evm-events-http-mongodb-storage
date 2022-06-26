8a1341ed1f5100b92ee023d38ba214fc8a5e213b
Luis Masuelli <luis@datagran.io>
2022-06-26 15:57:18 -0500

    Adding all the logic for the event grabbing worker.

--- alephvault/evm_http_storage/methods.py
+++ alephvault/evm_http_storage/methods.py
@@ -1,6 +1,13 @@
+import os
+import logging
+from flask import jsonify
+from alephvault.evm_http_storage.core import loop
 from alephvault.evm_http_storage.validation import WorkerSettingsValidator
 from alephvault.http_storage.types.method_handlers import MethodHandler
 from pymongo import MongoClient
+
+
+LOGGER = logging.getLogger(__name__)
 
 
 class EventGrabberWorker(MethodHandler):
@@ -10,24 +17,35 @@ class EventGrabberWorker(MethodHandler):
     state, processes them, and updates the state in the blockchain.
     """
 
-    def __init__(self, events_settings: dict):
+    def __init__(self, grabber_settings: dict):
         """
         Creates an event grabber handler by using certain events settings.
-        :param events_settings: The events settings to use.
+        :param grabber_settings: The events settings to use.
         """
 
-        # self._events_settings = events_settings
-        # validator = WorkerSettingsValidator()
-        # output = validator.validate(events_settings)
+        validator = WorkerSettingsValidator()
+        valid = validator.validate(grabber_settings)
+        if not valid:
+            raise ValueError(validator.errors)
+        self._events_settings = grabber_settings['events']
+        self._gateway_url = os.environ[self._events_settings['gateway_url_environment_var']]
 
     def __call__(self, client: MongoClient, resource: str, method: str, db: str, collection: str, filter: dict):
         """
-
-        :param client:
-        :param resource:
-        :param method:
-        :param db:
-        :param collection:
-        :param filter:
-        :return:
+        Processes a request to get the last events and process them.
+        :param client: The MongoDB client to use.
+        :param resource: The name of the related resource. Not used here.
+        :param method: The name of the related method. Not used here.
+        :param db: The name of the related resource's database.
+        :param collection: The name of the related resource's collection.
+        :param filter: The filter of the related resource. Not used here.
+        :return: A pair of processed element and an HTTP code: 200 if no error, 500 if error.
         """
+
+        processed, error = loop(gateway_url=self._gateway_url, events_settings=self._events_settings,
+                                client=client, cache_db=db, cache_state_collection=collection)
+        if error:
+            LOGGER.error(f"An error occurred while running the loop: {type(error).__name__} -> {error}")
+            return jsonify(processed), 200
+        else:
+            return jsonify(processed), 500
