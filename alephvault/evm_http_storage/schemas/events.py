aeb6b08e053fe487f0353d09de25c8bff54cd30a
Luis Masuelli <luis@datagran.io>
2022-06-26 12:16:00 -0500

    Moving some classes to avoid circular references.

--- alephvault/evm_http_storage/schemas/events.py
+++ alephvault/evm_http_storage/schemas/events.py
@@ -60,3 +60,16 @@ EVENTS_SETTINGS = {
         "schema": EVENT
     }
 }
+
+
+# This schema is meant for the worker loop. It will be
+# validated with the WorkerSettingsValidator class.
+WORKER_SCHEMA = {
+    "events": EVENTS_SETTINGS,
+    "gateway_url_environment_var": {
+        "type": "string",
+        "required": True,
+        "regex": r"[a-zA-Z_][a-zA-Z0-9_]",  # The url itself will satisfy: r"https?://[\w_-]+(\.[\w_-]+)*(:\d+)/?"
+        "default": "GATEWAY_URL"
+    }
+}
