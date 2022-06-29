bacb8a664f9c8a2a414060d32d622609120760e9
Luis Masuelli <luis@datagran.io>
2022-06-28 23:16:38 -0500

    The sample events, in the sample server, are changed for sample contracts instead.

--- samples/server.py
+++ samples/server.py
@@ -1,22 +1,22 @@
 import logging
 from alephvault.http_storage.flask_app import StorageApp
-from alephvault.evm_http_storage.core.common.token.shortcuts import erc20_transfer_event, erc721_transfer_event, \
-    erc1155_transfer_single_event, erc1155_transfer_batch_event
+from alephvault.evm_http_storage.core.common.token.shortcuts import erc20_schema_entry, erc721_schema_entry, \
+    erc1155_schema_entry
 from alephvault.evm_http_storage.schemas.resources import make_standard_evm_resources
 
 logging.basicConfig()
 
+# These are only sample values in a sample deployment.
 ERC1155 = "0x83fD6d68C21c5a646971F8a788f3992365321304"
 ERC721 = "0x0d938498e6C47DCc1c5a59Abc8aEf0eab7D115f4"
 ERC20 = "0xb6eFBbc5466693166E25F17998ec0A90263C5582"
 
 RESOURCES = make_standard_evm_resources({
-    'events': {
-        'erc20-transfer': erc20_transfer_event(ERC20, 'erc20-sample'),
-        'erc721-transfer': erc721_transfer_event(ERC721, 'erc721-sample'),
-        'erc1155-transfer-single': erc1155_transfer_single_event(ERC1155, 'erc1155-sample'),
-        'erc155-transfer-batch': erc1155_transfer_batch_event(ERC1155, 'erc1155-sample')
-    }
+    'contracts': [
+        erc20_schema_entry(ERC20, 'erc20-sample'),
+        erc721_schema_entry(ERC721, 'erc721-sample'),
+        erc1155_schema_entry(ERC1155, 'erc1155-sample')
+    ]
 })
 
 
